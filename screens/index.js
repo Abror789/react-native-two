8e1ff2c4da114af4c596893745964d4d60778965
Acer <abrorbek.m1974@gmail.com>
2021-10-30 14:08:10 +0500

    add new files

---
+++ screens/index.js
@@ -0,0 +1,9 @@
+import Onboarding from "./Onboarding";
+import Home from "./Home";
+import DestinationDetail from "./DestinationDetail";
+
+export {
+    Home,
+    Onboarding,
+    DestinationDetail
+}
